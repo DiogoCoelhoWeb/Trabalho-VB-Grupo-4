3a9f0e91204a32dec13f6e9784507acc448b48f4
Diogo Coelho <70858461+DiogoCoelhoWeb@users.noreply.github.com>
2022-04-05 12:34:13 +0100

    Design all of this ugly app

--- TarabalhoVB/Form1.Designer.vb
+++ TarabalhoVB/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class FrmTrabalho
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,45 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.PanelControl = New System.Windows.Forms.Panel()
+        Me.BtnClose = New System.Windows.Forms.Button()
+        Me.PanelControl.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PanelControl
+        '
+        Me.PanelControl.Controls.Add(Me.BtnClose)
+        Me.PanelControl.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PanelControl.Location = New System.Drawing.Point(0, 0)
+        Me.PanelControl.Name = "PanelControl"
+        Me.PanelControl.Size = New System.Drawing.Size(800, 40)
+        Me.PanelControl.TabIndex = 0
+        '
+        'BtnClose
+        '
+        Me.BtnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnClose.Location = New System.Drawing.Point(694, 3)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(94, 29)
+        Me.BtnClose.TabIndex = 0
+        Me.BtnClose.Text = "Button1"
+        Me.BtnClose.UseVisualStyleBackColor = True
+        '
+        'FrmTrabalho
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
-    End Sub
+        Me.ControlBox = False
+        Me.Controls.Add(Me.PanelControl)
+        Me.Name = "FrmTrabalho"
+        Me.PanelControl.ResumeLayout(False)
+        Me.ResumeLayout(False)
 
+    End Sub
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents PanelControl As Panel
+    Friend WithEvents BtnClose As Button
 End Class
--- TarabalhoVB/Form1.vb
+++ TarabalhoVB/Form1.vb
@@ -1,3 +1,5 @@
-﻿Public Class Form1
-
+﻿Public Class FrmTrabalho
+    Private Sub SairToolStripMenuItem_Click(sender As Object, e As EventArgs) 
+        Application.Exit()
+    End Sub
 End Class
